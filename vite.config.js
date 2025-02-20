2e06c5145a316eaf23091d646a559143f1183102
sanmato <santiago.mato.rpc@gmail.com>
2025-02-20 12:46:03 -0300

    Prueba deploy

--- vite.config.js
+++ vite.config.js
@@ -4,19 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    loader: "jsx",
-  },
-  resolve: {
-    alias: {
-      "./runtimeConfig": "./runtimeConfig.browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        ".js": "jsx",
-      },
-    },
-  },
 });
